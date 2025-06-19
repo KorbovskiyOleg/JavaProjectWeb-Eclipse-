9a41c6a33672fe2438e9b9c80ff1a9345417dd24
Car_Bit <olegkorbovskij@gmail.com>
2025-06-20 02:53:28 +0300

    Добавил даныее по авто в run

--- cardatabase/src/main/java/com/olegkorbovskij/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/CardatabaseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.olegkorbovskij.cardatabase.domain.Car;
 import com.olegkorbovskij.cardatabase.domain.CarRepository;
 
 @SpringBootApplication
@@ -25,7 +26,14 @@ public class CardatabaseApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		 repository.save(new Car("Ford", "Mustang", "Red", 
+		            "ADF-1121", 2021, 59000));
+		 repository.save(new  Car("Nissan", "Leaf", 
+				 				"White", 
+				 				"SSJ-3002", 2019, 29000));
+		 repository.save(new Car("Toyota", "Prius", 
+		            "Silver", 
+		            "KKO-0212", 2020, 39000));
 		
 	}
 
