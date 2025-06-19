732ecbca720504f61a676065e6b79b4b9abaf627
Car_Bit <olegkorbovskij@gmail.com>
2025-06-20 02:13:33 +0300

    Добавил новые свойства в application.property

--- cardatabase/src/main/java/com/olegkorbovskij/cardatabase/domain/Car.java
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/domain/Car.java
@@ -1,9 +1,10 @@
 package com.olegkorbovskij.cardatabase.domain;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.Id;;
 
 @Entity
 public class Car {
@@ -12,18 +13,18 @@ public class Car {
 	    @GeneratedValue(strategy=GenerationType.AUTO)
 	    private long id;
 	 	private String brand, model, color, registerNumber;
-	 	private int year, price;
+	 	private int yearOfCar, price;
 	 	
 	 	
 	 	public Car() {}
 	 	
-		public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+		public Car(String brand, String model, String color, String registerNumber, int yearOfCar, int price) {
 			super();
 			this.brand = brand;
 			this.model = model;
 			this.color = color;
 			this.registerNumber = registerNumber;
-			this.year = year;
+			this.yearOfCar = yearOfCar;
 			this.price = price;
 		}
 
@@ -68,11 +69,11 @@ public class Car {
 		}
 
 		public int getYear() {
-			return year;
+			return yearOfCar;
 		}
 
-		public void setYear(int year) {
-			this.year = year;
+		public void setYear(int yearOfCar) {
+			this.yearOfCar = yearOfCar;
 		}
 
 		public int getPrice() {
