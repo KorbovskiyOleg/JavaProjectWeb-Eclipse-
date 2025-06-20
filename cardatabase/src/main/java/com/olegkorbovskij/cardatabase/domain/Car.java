3eea4b4044228df722ee90f4c2ad584967fdd349
Car_Bit <olegkorbovskij@gmail.com>
2025-06-20 03:03:34 +0300

    Обновил класс сущности

--- cardatabase/src/main/java/com/olegkorbovskij/cardatabase/domain/Car.java
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/domain/Car.java
@@ -17,7 +17,8 @@ public class Car {
 	 	
 	 	
 	 	public Car() {}
-	 	
+
+
 		public Car(String brand, String model, String color, String registerNumber, int yearOfCar, int price) {
 			super();
 			this.brand = brand;
@@ -28,61 +29,77 @@ public class Car {
 			this.price = price;
 		}
 
+
 		public long getId() {
 			return id;
 		}
+
 
 		public void setId(long id) {
 			this.id = id;
 		}
 
+
 		public String getBrand() {
 			return brand;
 		}
+
 
 		public void setBrand(String brand) {
 			this.brand = brand;
 		}
 
+
 		public String getModel() {
 			return model;
 		}
+
 
 		public void setModel(String model) {
 			this.model = model;
 		}
 
+
 		public String getColor() {
 			return color;
 		}
+
 
 		public void setColor(String color) {
 			this.color = color;
 		}
 
+
 		public String getRegisterNumber() {
 			return registerNumber;
 		}
+
 
 		public void setRegisterNumber(String registerNumber) {
 			this.registerNumber = registerNumber;
 		}
 
-		public int getYear() {
+
+		public int getYearOfCar() {
 			return yearOfCar;
 		}
 
-		public void setYear(int yearOfCar) {
+
+		public void setYearOfCar(int yearOfCar) {
 			this.yearOfCar = yearOfCar;
 		}
+
 
 		public int getPrice() {
 			return price;
 		}
 
+
 		public void setPrice(int price) {
 			this.price = price;
 		}
+	 	
+		
 		
 		
 
