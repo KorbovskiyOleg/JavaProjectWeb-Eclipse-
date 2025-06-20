3df054a7dbc3636f74ecd9c9c79681cf91db7327
Car_Bit <olegkorbovskij@gmail.com>
2025-06-20 20:20:14 +0300

    Создал класс сущности Owner(владелец)

---
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/domain/Owner.java
@@ -0,0 +1,51 @@
+package com.olegkorbovskij.cardatabase.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Owner {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	
+	public Owner() {}
+
+	public Owner(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+
+}
