1bc6960e6fc65949c7a2a6bc852282e4659086c5
Car_Bit <olegkorbovskij@gmail.com>
2025-06-25 01:20:24 +0300

    Добавил метод getCars, который возвращает все имеющиеся авто в бд

--- cardatabase/src/main/java/com/olegkorbovskij/cardatabase/web/CarController.java
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/web/CarController.java
@@ -1,8 +1,21 @@
 package com.olegkorbovskij.cardatabase.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.olegkorbovskij.cardatabase.domain.Car;
+import com.olegkorbovskij.cardatabase.domain.CarRepository;
 
 @RestController
 public class CarController {
+	
+	@Autowired
+	private CarRepository repository;
+	
+	@RequestMapping("/cars")
+	public Iterable<Car> getCars() {
+		return repository.findAll();
+	}
 
 }
