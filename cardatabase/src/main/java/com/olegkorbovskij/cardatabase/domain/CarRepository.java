e7c8ff62885057d9abcc3a9100e816652ed2653f
Car_Bit <olegkorbovskij@gmail.com>
2025-06-25 06:00:55 +0300

    Добавил методы в rest запрос URL строки

--- cardatabase/src/main/java/com/olegkorbovskij/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/domain/CarRepository.java
@@ -3,12 +3,18 @@ package com.olegkorbovskij.cardatabase.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
 	// Получить авто по бренду
-	List<Car> findByBrand(String brand);
+	List<Car> findByBrand(@Param("brand")String brand);
 
 	// Получить авто по бредну и цвету
 	List<Car> findByBrandAndModel(String brand, String model);
+	
+	List<Car> findByColor(@Param("color") String color);
 
 }
