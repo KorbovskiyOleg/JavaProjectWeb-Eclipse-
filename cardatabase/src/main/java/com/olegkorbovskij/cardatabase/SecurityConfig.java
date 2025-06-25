5315216c64c130f4836b7ebf435c638e37b9a161
Car_Bit <olegkorbovskij@gmail.com>
2025-06-25 09:46:54 +0300

    Добавил user

--- cardatabase/src/main/java/com/olegkorbovskij/cardatabase/SecurityConfig.java
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/SecurityConfig.java
@@ -1,17 +1,38 @@
 package com.olegkorbovskij.cardatabase;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends  WebSecurityConfiguration{
+public class SecurityConfig extends WebSecurityConfiguration {
 	
-	protected void configure(HttpSecurity http) throws Exception {
-		
-	}
+   protected void configure(HttpSecurity http) throws  Exception {
+	   
+   }
+   
+   @Bean
+   public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+       UserDetails user = User.builder()
+           .username("user")
+           .password(encoder.encode("password")) // Шифрование пароля
+           .roles("USER")
+           .build();
+       return new InMemoryUserDetailsManager(user);
+   }
 
+   @Bean
+   public PasswordEncoder passwordEncoder() {
+       return new BCryptPasswordEncoder(); // BCrypt-шифрование
+   }
 }
