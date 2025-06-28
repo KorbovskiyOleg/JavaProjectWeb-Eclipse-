19ebacbdaefa45b12658951a53559f51c2ab98d4
Car_Bit <olegkorbovskij@gmail.com>
2025-06-28 06:52:30 +0300

    Основное: изменил названия метода, добавил настройку JPA/Hibernate для автоматического экранирования идентификаторов

--- cardatabase/src/main/java/com/olegkorbovskij/cardatabase/domain/OwnerRepository.java
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/domain/OwnerRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 	
-	 Optional<Owner> findByFirstname(String firstName);
+	 Optional<Owner> findByFirstName(String firstName);
 
 }
--- cardatabase/src/test/java/com/olegkorbovskij/cardatabase/OwnerRepositoryTest.java
+++ cardatabase/src/test/java/com/olegkorbovskij/cardatabase/OwnerRepositoryTest.java
@@ -3,6 +3,7 @@ package com.olegkorbovskij.cardatabase;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,21 +15,23 @@ import com.olegkorbovskij.cardatabase.domain.OwnerRepository;
 public class OwnerRepositoryTest {
 	
 	 @Autowired
-     private OwnerRepository repository;
+     private OwnerRepository orepository;
 	 
 	 @Test
+	 @DisplayName("Two example test case")
 	 void saveOwner() {
-	  repository.save(new Owner("Lucy", "Smith"));
-	  assertThat(repository.findByFirstname
+	  orepository.save(new Owner("Lucy", "Smith"));
+	  assertThat(orepository.findByFirstName
 	      ("Lucy").isPresent())
 	    .isTrue();
 	 }
 	 
 	 @Test
+	 @DisplayName("Three example test case")
 	 void deleteOwners() {
-	  repository.save(new Owner("Lisa", "Morrison"));
-	  repository.deleteAll();
-	  assertThat(repository.count()).isEqualTo(0);
+	  orepository.save(new Owner("Lisa", "Morrison"));
+	  orepository.deleteAll();
+	  assertThat(orepository.count()).isEqualTo(0);
 	 }
 
 }
