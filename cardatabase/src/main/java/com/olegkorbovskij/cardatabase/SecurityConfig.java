8fce38b3b10101da9adc1ca4a8e26e872bff3da8
Car_Bit <olegkorbovskij@gmail.com>
2025-06-28 03:41:09 +0300

    Корректировка кода

--- cardatabase/src/main/java/com/olegkorbovskij/cardatabase/SecurityConfig.java
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.olegkorbovskij.cardatabase;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,7 +20,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import com.olegkorbovskij.cardatabase.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +32,13 @@ public class SecurityConfig {
 
 	@Autowired
 	private AuthEntryPoint exceptionHandler;
+	
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+	
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
 
 
 
