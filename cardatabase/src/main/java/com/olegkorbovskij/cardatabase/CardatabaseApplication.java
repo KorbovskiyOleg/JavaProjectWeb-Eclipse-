a28eaad0ae71b3088d4b7f4b739377dd4f90d85c
Car_Bit <olegkorbovskij@gmail.com>
2025-06-28 13:58:14 +0300

    Проверка слияния

--- cardatabase/src/main/java/com/olegkorbovskij/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/CardatabaseApplication.java
@@ -56,6 +56,7 @@ public class CardatabaseApplication implements CommandLineRunner {
 			logger.info(car.getBrand() + " " + car.getModel() + " " + car.getId());
 		}
 
+
 		// User 1(Username: user, password: user)
 		// User 2(Username: admin, password: admin)
 		urepository.save(new User("user", "$2a$10$0mbQIzQXksZT4tkHzI4rw.0BPb13Twv.Wz8cxdwFHOhBW5IGA/PRm", "USER"));
