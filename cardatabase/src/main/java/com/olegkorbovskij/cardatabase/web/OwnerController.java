77c5ac54d8494d8f55afa4df2d496a5d4d5684e5
Car_Bit <olegkorbovskij@gmail.com>
2025-08-05 07:20:25 +0300

    добавили контроллер владельцев авто

---
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/web/OwnerController.java
@@ -0,0 +1,21 @@
+package com.olegkorbovskij.cardatabase.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.olegkorbovskij.cardatabase.domain.Owner;
+import com.olegkorbovskij.cardatabase.domain.OwnerRepository;
+
+@RestController
+public class OwnerController {
+	
+	@Autowired
+	private OwnerRepository repository;
+	
+	@RequestMapping("/owners")
+	public Iterable<Owner> getOwner() {
+		return repository.findAll();
+	}
+
+}
