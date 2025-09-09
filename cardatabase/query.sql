304a6bcaeb82faf9511cf1ec45a110d1007d9425
Car_Bit <olegkorbovskij@gmail.com>
2025-09-09 11:48:39 +0300

    добавил SQL запрос

--- cardatabase/query.sql
+++ cardatabase/query.sql
@@ -1,3 +1,7 @@
-SELECT * FROM car;
+SELECT * FROM car; -- запрос всех данных из таблицы
 SELECT * FROM owner;
 SELECT *from app_users;
+DELETE FROM note WHERE note_id = 6;-- удаление по ID
+DELETE FROM note; -- удаление всех данных из таблицы
+
+
