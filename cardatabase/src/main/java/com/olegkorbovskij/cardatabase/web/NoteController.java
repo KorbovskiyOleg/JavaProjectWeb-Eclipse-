043f18b4c229b69a2efd4c30881a50490f263556
Car_Bit <olegkorbovskij@gmail.com>
2025-09-14 09:43:09 +0300

    вернулся к состоянию без контороллера

--- cardatabase/src/main/java/com/olegkorbovskij/cardatabase/web/NoteController.java
+++ cardatabase/src/main/java/com/olegkorbovskij/cardatabase/web/NoteController.java
@@ -1,29 +1,37 @@
-package com.olegkorbovskij.cardatabase.web;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.olegkorbovskij.cardatabase.domain.Note;
-import com.olegkorbovskij.cardatabase.domain.NoteRepository;
-
-@RestController
-@RequestMapping("/api")
-public class NoteController {
-	
-	@Autowired
-	private NoteRepository noteRepository;
-	
-	@GetMapping("/notes")
-	public Iterable<Note> getNote(){
-		return noteRepository.findAll();
-	}
-	
-
-	
-	
-	
-	
-
-}
+//package com.olegkorbovskij.cardatabase.web;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.olegkorbovskij.cardatabase.domain.Note;
+//import com.olegkorbovskij.cardatabase.domain.NoteRepository;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class NoteController {
+//	
+//	@Autowired
+//	private NoteRepository noteRepository;
+//	
+//	@GetMapping("/notes")
+//	public Iterable<Note> getNote(){
+//		return noteRepository.findAll();
+//	}
+//	
+//	@PostMapping("/notes")
+//    //@ResponseStatus(HttpStatus.CREATED)
+//    public Note createNote(@RequestBody Note note) {
+//        return noteRepository.save(note);
+//    }
+//
+//	
+//	
+//	
+//	
+//
+//}
