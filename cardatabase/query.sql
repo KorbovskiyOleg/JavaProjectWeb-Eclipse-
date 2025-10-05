f722d37c65987ea05f8f56472842ade2ec6edcc5
Car_Bit <olegkorbovskij@gmail.com>
2025-10-05 03:28:05 +0300

    создание новой схемы ml_data

--- cardatabase/query.sql
+++ cardatabase/query.sql
@@ -3,3 +3,6 @@ SELECT * FROM owner;
 SELECT *from app_users;
 DELETE FROM note WHERE note_id = 4;-- удаление по ID
 DELETE FROM note; -- удаление всех данных из таблицы
+INSERT INTO note (content_note, name_note) VALUES ('Название заметки', 'Текст заметки');
+CREATE SCHEMA IF NOT EXISTS ml_data AUTHORIZATION postgres; -- создание новой схемы ml_data
+
